847429597cd8b1168c99fa13d28ec4c60f7024ac
ryler <ryan.leroux77@outlook.com>
2026-09-10 20:14:33 +0000

    More Part 3 progress

--- EduMax/app.py
+++ EduMax/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def default():
-    return render_template("../pages/form.html")
+    return render_template("form.html")
     
 
 if __name__ == '__main__':
